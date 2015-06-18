2408fced62f45709028374756b0823fc4cc74e3a
kucats <info@ko.gs>
2015-06-18 14:33:30 +0900

    add Class RSS_Parse
    仮のパーサーを入れた

--- api/rsstojson.php
+++ api/rsstojson.php
@@ -1,15 +1,40 @@
 <?php
+//!!deprecated module
 require_once "XML/RSS.php";
 
-$rss =& new XML_RSS("http://rss.slashdot.org/Slashdot/slashdot");
-$rss->parse();
+Class RSS_Parse(){
+	//mongoを作るまでの仮の変数
+	public $db=array();
 
-echo "<h1>Headlines from <a href=\"http://slashdot.org\">Slashdot</a></h1>\n";
-echo "<ul>\n";
+	function registerItem($array){
+		if(isset($array['title']) && isset($array['link'])){
+			$db[]=$array;
+		}else{
+			return false;
+		}
+	}
 
-foreach ($rss->getItems() as $item) {
-    echo "<li><a href=\"" . $item['link'] . "\">" . $item['title'] . "</a></li>\n";
+	function getRSS($category){
+		$url =$this->makeURI($category);
+
+		$rss =& new XML_RSS($url);
+		$rss->parse();
+		foreach ($rss->getItems() as $item) {
+		$this->registerItem('title'=>$item['title'],'link'=>$item['link']);
+		}
+	}
+	function makeURI($category){
+		$base_url = Constants::base_url;
+		$base_ext = Constants::base_ext;
+		return $base_url.$category.$base_ext;
+	}
 }
-
-echo "</ul>\n";
+Class Constatnts{
+	//起点となるURL
+	public $base_url='http://news.livedoor.com/topics/rss/';
+	//拡張子
+	public $base_ext='.xml';
+	//カテゴリ
+	public $rss_category=array('top'=>'主要','dom'=>'国内','int'=>'海外','eco'=>'IT経済','ent'=>'芸能','spo'=>'スポーツ','52'=>'映画','gourmet'=>'グルメ','love'=>'女子','trend'=>'トレンド');
+}
 ?>
