b0440771fddb2351290548b52e0ff1736acee4a1
kucats <info@ko.gs>
2015-06-18 17:56:43 +0900

    Add DB_MYSQL class

---
+++ api/dbconn.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(./constants.php);
+
+Class DB_MYSQL(){
+	public $dbh;
+
+	function __construct(){
+		$hostname=Constants::MYSQL_ATTR_Hostname;
+		$dsn = "mysql:host=$hostname;dbname=$dbname";
+		$username = Constants::MYSQL_ATTR_USERNAME;
+		$password = Constants::MYSQL_ATTR_PASSWORD;
+		$options = array(
+		    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+		); 
+		$dbh = new PDO($dsn, $username, $password, $options);
+
+		$this->dbh=$dbh;
+	}
+}
+?>
